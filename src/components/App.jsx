ee63d39c52651ada90c9f125ab0b22c8d1348e66
Ihor <kovaluk_igor@ukr.net>
2024-02-08 13:03:22 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import { Component } from 'react'
-import { nanoid } from 'nanoid'
-import Phonebook from 'components/Searchbar/Searchbar'
-import Contacts from 'components/ImageGalleryItem/ImageGalleryItem'
-import Filter from './ImageGallery/ImageGallery'
-import css from './App.module.css'
+
+import Searchbar from 'components/Searchbar/Searchbar'
+// import Contacts from 'components/ImageGalleryItem/ImageGalleryItem'
+// import Filter from './ImageGallery/ImageGallery'
+// import css from './App.module.css'
 
 
 class App extends Component {
 
   state = {
-
+    value: '',
   }
 
   componentDidMount() {
@@ -20,12 +20,25 @@ class App extends Component {
    
   }
 
+  handleChange= (evt) => {
+    this.setState({ value: evt.target.value })
+
+  }
+
+  handleSubmit = (evt) => {
+
+    evt.preventDefault()
+    console.log(this.state.value);
+    this.setState({ value: '' })
+   }
+
+
   render() {
 
       
     return (
       <>
-       
+        <Searchbar value={this.state.value} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
         
       </>
 
