4ea89eebfa31ff921ef91a8e49ae704f6161bd9a
Ihor <kovaluk_igor@ukr.net>
2024-02-08 13:03:37 +0100

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import css from './Phonebook.module.css'
+// import css from './Phonebook.module.css'
 
 
 class Searchbar extends Component { 
@@ -8,7 +8,23 @@ class Searchbar extends Component {
        
         return (
             <>
-                
+                <header className="searchbar">
+                    <form className="form" onSubmit={this.props.handleSubmit}>
+                        <button type="submit" className="button">
+                        <span className="button-label">Search</span>
+                        </button>
+
+                        <input
+                        className="input"
+                        type="text"
+                        autoComplete="off"
+                        autoFocus
+                        placeholder="Search images and photos"
+                        onChange={this.props.handleChange}
+                        value={this.props.value}
+                        />
+                    </form>
+</header>
                 
             </>
         )
