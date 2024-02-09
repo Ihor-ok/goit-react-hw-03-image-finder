68141dec6042e33436b59356fc3a4975dd0f3394
Ihor <kovaluk_igor@ukr.net>
2024-02-09 01:34:56 +0100

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,12 +1,17 @@
-// import css from './ImageGalleryItem.module.css'
+import css from './ImageGalleryItem.module.css'
 
-// export default function ImageGalleryItem({ }) {
+export default function ImageGalleryItem({imgs}) {
 
-//   return (
-//     <>
+  return (
+    <>
+          {imgs.map((img) => {
+              return (<li key={img.id} className={css.galleryItem}>
+                        <img className={css.galleryItemImg} src={img.previewURL} alt={img.tags} />
+                      </li>)
+          })}
       
-//     </>
+    </>
         
 
-//   )
-// };
+  )
+};
