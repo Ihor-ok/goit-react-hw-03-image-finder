4e81da0c140c5635073584f59abea8b8fbf713f4
Ihor <kovaluk_igor@ukr.net>
2024-02-10 01:47:27 +0100

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-// import css from './Phonebook.module.css'
+import css from './Searchbar.module.css'
 
 
 class Searchbar extends Component { 
@@ -8,14 +8,17 @@ class Searchbar extends Component {
        
         return (
             <>
-                <header className="searchbar">
-                    <form className="form" onSubmit={this.props.handleSubmit}>
-                        <button type="submit" className="button">
-                        <span className="button-label">Search</span>
+                <header className={css.Searchbar}>
+                    <form className={css.SearchForm} onSubmit={this.props.handleSubmit}>
+                        <button type="submit" className={css.SearchFormButton}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.94-5-5.78-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.84 2.56 5.31 5.34 5.78a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                            </svg>
                         </button>
 
                         <input
-                        className="input"
+                        className={css.SearchFormInput}
                         type="text"
                         autoComplete="off"
                         autoFocus
